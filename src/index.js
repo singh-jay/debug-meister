a05cd9a3bcc2f095d8128546a8ac75bc06aca834
Jay Singh <jay.singh@infoedge.com>
2023-07-24 13:11:27 +0530

    bump version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import { useState } from 'react';
 import './styles/globals.css';
-import { NetworkProvider } from './context/NetworkProvider';
+// import { NetworkProvider } from './context/NetworkProvider';
+
+const NetworkProvider = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex gap-2 text-xl text-red">
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      {count}
+      <button onClick={() => setCount((c) => c - 1)}>-</button>
+    </div>
+  );
+};
 
 export { NetworkProvider };
