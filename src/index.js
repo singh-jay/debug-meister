f2043c3d32def85090c1d7a9d0257a77fecee970
Jay Singh <jay.singh@infoedge.com>
2023-07-24 14:56:26 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 import './styles/globals.css';
-// import { NetworkProvider } from './context/NetworkProvider';
+import { NetworkProvider } from './context/NetworkProvider';
 
-const NetworkProvider = ({ children }) => {
-  const [count, setCount] = useState(0);
+// const NetworkProvider = ({ children }) => {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <div className="flex gap-2 bg-white text-2xl text-red">
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
-      {count}
-      <button onClick={() => setCount((c) => c - 1)}>-</button>
-      {children}
-    </div>
-  );
-};
+//   return (
+//     <div className="flex gap-2 bg-white text-2xl text-red">
+//       <button onClick={() => setCount((c) => c + 1)}>+</button>
+//       {count}
+//       <button onClick={() => setCount((c) => c - 1)}>-</button>
+//       {children}
+//     </div>
+//   );
+// };
 
 export { NetworkProvider };
