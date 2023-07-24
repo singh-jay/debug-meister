96e57d5e9a4ef43c456776495c4e3cada82d73a1
Jay Singh <jay.singh@infoedge.com>
2023-07-24 12:43:43 +0530

    make client only

--- src/context/NetworkProvider.js
+++ src/context/NetworkProvider.js
@@ -1,33 +1,43 @@
-"use client";
+'use client';
 
-import React, { useCallback, useState } from "react";
-import { NetworkContext } from "./NetworkContext";
-import withNetworkLogger from "../components/HOC/withNetworkLogger";
-import DebugOverlay from "../components/DebugOverlay";
+import React, { useCallback, useEffect, useState } from 'react';
+import { NetworkContext } from './NetworkContext';
+import withNetworkLogger from '../components/HOC/withNetworkLogger';
+import DebugOverlay from '../components/DebugOverlay';
 
 const Child = ({ children }) => (
-	<>
-		<DebugOverlay />
-		{children}
-	</>
+  <>
+    <DebugOverlay />
+    {children}
+  </>
 );
 
 const WrappedComponent = withNetworkLogger(Child);
 
 export const NetworkProvider = ({ children }) => {
-	const [networkRequests, setNetworkRequests] = useState([]);
+  const [mounted, setMounted] = useState(second);
+  const [networkRequests, setNetworkRequests] = useState([]);
 
-	const addNetworkRequest = useCallback((request) => {
-		setNetworkRequests((prevNetworkRequests) => [...prevNetworkRequests, request]);
-	}, []);
+  const addNetworkRequest = useCallback((request) => {
+    setNetworkRequests((prevNetworkRequests) => [
+      ...prevNetworkRequests,
+      request,
+    ]);
+  }, []);
 
-	const clearNetworkRequests = useCallback(() => {
-		setNetworkRequests([]);
-	}, []);
+  const clearNetworkRequests = useCallback(() => {
+    setNetworkRequests([]);
+  }, []);
 
-	return (
-		<NetworkContext.Provider value={{ networkRequests, addNetworkRequest, clearNetworkRequests }}>
-			<WrappedComponent>{children}</WrappedComponent>
-		</NetworkContext.Provider>
-	);
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return (
+    <NetworkContext.Provider
+      value={{ networkRequests, addNetworkRequest, clearNetworkRequests }}
+    >
+      <WrappedComponent>{children}</WrappedComponent>
+    </NetworkContext.Provider>
+  );
 };
